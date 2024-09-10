66d8be64ba8b64ec890d8ce4e242199afe143e5f
CafeCat101 <leonore.yardimli@icloud.com>
2024-09-10 18:44:38 +0800

    Initial Commit

---
+++ TestWordToggle/TestWordToggleApp.swift
@@ -0,0 +1,20 @@
+//
+//  TestWordToggleApp.swift
+//  TestWordToggle
+//
+//  Created by Leonore Yardimli on 2024/9/10.
+//
+
+import SwiftUI
+
+@main
+struct TestWordToggleApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
